790dd97b389e6d5400faf91bfef8179046496709
hamasabdulkareem <46087481+hamasabdulkareem@users.noreply.github.com>
2020-09-22 09:36:05 +0300

    add page

---
+++ js/custums.js
@@ -0,0 +1,60 @@
+var yemencities="<option value='sana'>sana</option><option value='tiaz'>tiaz</option><option value='Aden'>Aden</option>";
+var egyptcities="<option value='Cairo'>Cairo</option><option value='Alexandria'>Alexandria</option>";
+var cities=document.getElementById("cities") ;
+var country=document.getElementById("Country");
+function changcity()
+{
+    if(country.value=="yemen") 
+    {
+        cities.innerHTML=yemencities ;
+    }
+    else
+    {
+        cities.innerHTML=egyptcities;
+    }
+}
+$(document).ready(function()
+{
+
+
+if($('.bbb_viewed_slider').length)
+{
+var viewedSlider = $('.bbb_viewed_slider');
+
+viewedSlider.owlCarousel(
+{
+loop:true,
+margin:30,
+autoplay:true,
+autoplayTimeout:6000,
+nav:false,
+dots:false,
+responsive:
+{
+0:{items:1},
+575:{items:2},
+768:{items:3},
+991:{items:4},
+1199:{items:6}
+}
+});
+
+if($('.bbb_viewed_prev').length)
+{
+var prev = $('.bbb_viewed_prev');
+prev.on('click', function()
+{
+viewedSlider.trigger('prev.owl.carousel');
+});
+}
+
+if($('.bbb_viewed_next').length)
+{
+var next = $('.bbb_viewed_next');
+next.on('click', function()
+{
+viewedSlider.trigger('next.owl.carousel');
+});
+}
+}
+});
